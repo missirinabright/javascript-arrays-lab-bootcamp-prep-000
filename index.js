d9c2870127cc190810417745d80349a04df335d4
Irina Bright <missirinabright@gmail.com>
2019-04-19 02:57:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten() {
+    return kittens.push('Ralph');
+  }
+
+function destructivelyPrependKitten() {
+    return kittens.unshift('Bob');
+  }
+  
+  function destructivelyRemoveLastKitten() {
+    return kittens.pop();
+  }
+  
+  function destructivelyRemoveFirstKitten() {
+    return kittens.shift();
+  }
+  
+  function appendKitten() {
+    return (...kittens);
+  }
